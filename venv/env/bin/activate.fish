8a222b1d2e6e9515d4fa44ff7f0959ba275df4c8
Nick Bielinski <ndbielinski@gmail.com>
2018-01-30 20:09:16 -0500

    updated virtual environment to python 3.x

--- venv/env/bin/activate.fish
+++ venv/env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nbielinski/workspace/venvs/env"
+set -gx VIRTUAL_ENV "/home/nbielinski/workspace/venv/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
