929ca0e8e711f75bef9c40577fb5a2abbd8177a1
Nick Bielinski <ndbielinski@gmail.com>
2018-02-05 18:54:06 -0500

    added links and all links go to the correct pages

--- django/week2/portfolio/urls.py
+++ django/week2/portfolio/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
+    url(r'^$', views.home, name='home'),
+    url(r'^resume/$', views.resume, name='resume'),
+    url(r'^portfolio/$', views.portfolio, name='portfolio'),
+    url(r'^contact/$', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
--- django/week2/portfolio/views.py
+++ django/week2/portfolio/views.py
@@ -6,3 +6,24 @@ def home(request):
     """
     context = {}
     return render(request, 'home.html', context)
+
+def resume(request):
+    """
+    Renders the home page
+    """
+    context = {}
+    return render(request, 'resume.html', context)
+
+def portfolio(request):
+    """
+    Renders the home page
+    """
+    context = {}
+    return render(request, 'portfolio.html', context)
+
+def contact(request):
+    """
+    Renders the home page
+    """
+    context = {}
+    return render(request, 'contact.html', context)
