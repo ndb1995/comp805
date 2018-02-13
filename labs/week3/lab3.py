f5bb7be678dcc637b34e542dc7a837d347a4bc1f
Nick Bielinski <ndbielinski@gmail.com>
2018-02-13 13:11:24 -0500

    Completed the first two python practice functions, switch case and only even

--- labs/week3/lab3.py
+++ labs/week3/lab3.py
@@ -1,65 +1,130 @@
-def squared_nums(num_list):
+def switch_case(str_list):
     """
-    Squares numbers in num_list
+    Maps strings in the str_list to a new string of
+    same characters, but the first letter contains
+    the opposite case
+    string_list: list of strings
+    Returns: list of original strings, but with
+    opposite casing
+    """
+    string_list = []
+    for word in str_list:
+        string_list.append(word[0].capitalize() + word[1:])
+    return string_list
+
+def only_even(mixed_list):
+    """
+    Filters out odd integers and strings that
+    contain an odd number of characters.
+    mixed_list: list of integers and/or strings
+    Returns: list of only integers and strings that
+    are even or have an even number of characters.
+    """
+    even_list = []
+    for mixed in mixed_list:
+        if type(mixed) == int:
+            if mixed % 2 == 0:
+                even_list.append(mixed)
+        else:
+            if len(mixed) % 2 == 0:
+                even_list.append(mixed)
+    return even_list
+
+def greatest_difference(num_list):
+    """
+    Finds the maximum and minimum numbers in a_list
+    and computes the difference.
     num_list: list of numbers
-    Returns: list of these numbers squared_nums
+    Returns: the difference between the maximum and
+    minimum numbers in num_list
     """
 
-    squared_list = []
-    for number in num_list:
-        squared_list.append(number * number)
-    return(squared_list)
+def make_title(words):
+    """
+    Maps words in a list to words in the same list,
+    but as titled strings.
+    words: list of words
+    Returns:
+    """
 
-def check_title(title_list):
+def test_title(names):
     """
-    Removes strings in title_list that have numbers and aren't title case
-    title_list: list of strings
-    Returns: list of strings that are title_list
+    Filters out capitalized and non- cap words into
+    their respective lists.
+    names: list of names
+    Returns: both lists for review
     """
-    correct_list = []
-    for title in title_list:
-        if (type(title) != int):
-            if (title.istitle()):
-                correct_list.append(title)
-            else:
-                title_listtitle = ""
-    return(correct_list)
 
-def restock_inventory(inventory):
+def create_word(char_list):
     """
-    Increases inventory of each item in dictionary by 10
-    inventory: ad dictionary with:
-     key: string that is the name of the inventory item
-     value: integer that equals the number of that item currently on hand
-    Returns: updated dictionary where each inventory item is restocked
+    Takes a list of characters and creates a word
+    (list with alpha chars only) from them.
+    letters: list of etters
+    Returns: list that has alpha characters only
     """
-    for key, value in inventory.items():
-            inventory[key] = value + 10
-    return (inventory)
 
-def filter_0_items(inventory):
+def three_times_nums(num_list):
     """
-    Removes items that have a value of 0 from a dictionary of inventories
-    inventory: dictionary with:
-       key: tring that is the name of the inventory item
-       value: nteger that equals the number of that item currently on hand
-    Returns: the same inventory_dict with any item that had 0 quantity removed
+    Maps numbers in the num_list to numbers that are
+    3 times the original value
+    num_list: list of numbers
+    Returns: list of numbers that are of three times
+    the values in num_list
     """
-    for key in list(inventory.keys()):
-            if (inventory[key] == 0):
-                del inventory[key]
-    return (inventory)
 
-def average_grades(grades):
+def keep_lowercase(str_list):
     """
-    Takes grade values from a dictionary and averages them into a final grade
-    grades: a dictionary of grades with:
-    key: string of students name
-    value: list of integer grades received in class
-    Returns: dictionary that averages out the grades of each student
+    Filters out strings that have uppercase values
+    str_list: list of strings
+    Returns: list of strings that do not contain
+    uppercase values
     """
-    avg_grades = []
-    for key,value in grades.items():
-        avg_grades.append(key)
-        avg_grades.append(sum(value)/ float(len(value)))
-    return (avg_grades)
+
+def multiplication_total_of(num_list):
+    """
+    Multiplies all the numbers in num_list together
+    and gives the total
+    num_list: list of numbers
+    Returns: the multiplied total of the numbers in
+    the num_list
+    """
+
+def square_nums(number_list):
+    """
+    Maps numbers in the number_list to numbers of
+    same value, but squares the number given
+    number_list: list of numbers
+    Returns: list from number_list which are squared
+    """
+
+def lessthan_5(num_list):
+    """
+    Filters out numbers less than five
+    num_list: list of numbers
+    Returns: list of numbers in the original list
+    that are less than five
+    """
+
+def subtraction_of(number_list):
+    """
+    Subtracts the numbers in number_list
+    number_list: list of numbers
+    Returns: the difference of the numbers in the
+    number_list
+    """
+
+def double_nums(num_list):
+    """
+    Maps numbers in the num_list to their doubles
+    num_list: list of numbers
+    Returns: list of doubled numbers
+    """
+
+def remove_special_characters(string_list):
+    """
+    Filters out strings that have non-alphanumeric
+    elements
+    char_list: list of strings
+    Returns: list of strings that have only letters
+    or numbers in them
+    """
---
+++ labs/week3/tests.py
@@ -0,0 +1,7 @@
+import lab3
+
+print("\nFirst Function: Switch Case\n")
+print (lab3.switch_case(['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']))
+
+print("\nSecond Function: Only Even\n")
+print (lab3.only_even(['orange', 5, 'pear', 2, 'kiwi', 7, '10']))
