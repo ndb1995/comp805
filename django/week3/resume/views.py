0a4049d02d457043c70bcc860640890f22372387
Nick Bielinski <ndbielinski@gmail.com>
2018-02-12 19:53:40 -0500

    added to my experience and education templates, and added to my resume apps urls and views

--- django/week3/resume/views.py
+++ django/week3/resume/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+def education(request):
+    """
+    Renders the home page
+    """
+    context = {}
+    return render(request, 'education.html', context)
+
+def experience(request):
+    """
+    Renders the home page
+    """
+    context = {}
+    return render(request, 'experience.html', context)
