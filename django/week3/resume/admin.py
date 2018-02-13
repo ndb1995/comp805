fd1117ef61c19b6e2a9b8658a3b118941c1f79e0
Nick Bielinski <ndbielinski@gmail.com>
2018-02-12 19:42:07 -0500

    Removed admin panel showing objects, now displays attributes of the model

--- django/week3/resume/admin.py
+++ django/week3/resume/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from . models import education, experience
 
+
+class education_model(admin.ModelAdmin):
+    model = education
+    list_display = ('institution_name', 'location', 'degree', 'major', 'gpa')
+
 # Register your models here.
-admin.site.register(education)
+admin.site.register(education, education_model)
 admin.site.register(experience)
